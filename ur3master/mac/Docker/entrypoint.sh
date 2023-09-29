c79ca08b73b4ef44c1e6eeefb039805263e08e39
Nicholas <nbaddorf@gmail.com>
2023-09-29 12:20:41 -0400

    Got the communicating working

--- ur3master/mac/Docker/entrypoint.sh
+++ ur3master/mac/Docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+
+
 set -e
 
 source /opt/ros/melodic/setup.bash
@@ -11,8 +13,15 @@ export ROS_MASTER_URI=http://ur3robot.local:11311
 exec supervisord -c /supervisord.conf &
 
 echo "Provided arguments: $@"
+echo "############################"
+echo ""
+echo "http://localhost:8080/vnc_auto.html"
+echo ""
+echo "Must start Rviz, just type rviz. "
+echo "############################\n"
+
 
 exec $@
 
-echo "http://localhost:8080/vnc_auto.html"
+#echo "http://localhost:8080/vnc_auto.html"
 
