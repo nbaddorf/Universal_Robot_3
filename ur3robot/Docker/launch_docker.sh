d446bcc85ea0d794b98cbdd18e02fb4a78423d4a
Nicholas <nbaddorf@gmail.com>
2023-09-29 13:47:29 -0400

    Trying to figure out why ur3robot docker cant ping Nicksmacbook

--- ur3robot/Docker/launch_docker.sh
+++ ur3robot/Docker/launch_docker.sh
@@ -2,4 +2,5 @@
 
 #docker run -it --user ros --network=host --ipc=host ur3robot_docker_image
 #docker run -it --user ros --network=host --add-host ur3robot:ur3robot.local --ipc=host -v /home/$USER/Universal_Robot_3/ur3robot/ros_code:/home/ros/catkin_ws/src/ur3robot ur3robot_docker_image
-docker run -it --user ros --network=host --device=/dev/ttyUSB0 --ipc=host -v /home/$USER/Universal_Robot_3/ur3robot/ros_code:/home/ros/catkin_ws/src/ur3robot ur3robot_docker_image
+#docker run -it --user ros --network=host --device=/dev/ttyUSB0 --ipc=host -v /home/$USER/Universal_Robot_3/ur3robot/ros_code:/home/ros/catkin_ws/src/ur3robot ur3robot_docker_image
+docker run -it --user ros --network=host --device=/dev/ttyUSB0 -v /home/$USER/Universal_Robot_3/ur3robot/ros_code:/home/ros/catkin_ws/src/ur3robot ur3robot_docker_image
