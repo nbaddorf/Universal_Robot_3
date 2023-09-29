f28c66042cfba02ead1e97407b0c2ad53b16bf5b
Nicholas <nbaddorf@gmail.com>
2023-09-29 15:33:01 -0400

    update entrypoint.shj

--- ur3robot/Docker/entrypoint.sh
+++ ur3robot/Docker/entrypoint.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-export ROS_HOSTNAME=ur3robot
+export ROS_HOSTNAME=ur3robot.local
+export ROS_MASTER_URI=http://ur3robot.local:11311
 source /opt/ros/melodic/setup.bash
 
 sudo chmod 777 /dev/ttyUSB0
