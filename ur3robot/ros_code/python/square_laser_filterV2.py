3c5571355cebed79a90f860f9d324f5f20c0e2c7
Nicholas <nbaddorf@gmail.com>
2023-10-30 08:21:59 -0400

    Made a new laser filter that should be better

---
+++ ur3robot/ros_code/python/square_laser_filterV2.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+#import time
+#import math
+import numpy as np
+import rospy
+from sensor_msgs.msg import LaserScan
+
+#******* This program is only for square robots. ********
+
+robot_width = 0.4191 #my ur3 robot is 0.4191 meters width
+
+laser_lookup_table = [] #this will be where the initial startup math will calculate the minimum values for each position of laser values
+
+def create_lookup_table():
+    global robot_width
+    global laser_lookup_table
+
+    for deg in range(360):
+        if ((deg <= 45 or deg > 315) or (deg > 135 and deg <= 225)):
+            distance = (robot_width/2)/np.cos(np.radians(deg))
+            laser_lookup_table.append(abs(distance))
+        elif ((deg > 45 and deg <= 135) or (deg > 225 and deg <= 315)):
+            distance = (robot_width/2)/np.sin(np.radians(deg))
+            laser_lookup_table.append(abs(distance))
+    print("DONE")
+
+def run_filter(msg):
+    global laser_lookup_table
+
+    distances_array = []
+    intensities_array = []
+
+    for deg in range(360):
+
+        if msg.ranges[deg] <= laser_lookup_table[deg]:
+            distances_array.append(0)
+            intensities_array.append(0)
+        else:
+            distances_array.append(msg.ranges[deg])
+            intensities_array.append(msg.intensities[deg])
+    return distances_array, intensities_array
+
+def callback(msg):
+    new_ranges, new_intensities = run_filter(msg)
+    scan = LaserScan()
+    scan.header = msg.header
+    scan.angle_min = msg.angle_min
+    scan.angle_max = msg.angle_max
+    scan.angle_increment = msg.angle_increment
+    scan.time_increment = msg.time_increment
+    scan.scan_time = msg.scan_time
+    scan.range_min = msg.range_min
+    scan.range_max = msg.range_max
+    scan.ranges = new_ranges
+    scan.intensities = new_intensities
+    pub.publish(scan)
+        
+
+if __name__ == "__main__":
+    create_lookup_table()
+
+    rospy.init_node('laser_filter')
+    sub = rospy.Subscriber('/scan', LaserScan, callback)
+    pub = rospy.Publisher('scan/filtered', LaserScan, queue_size=5)
+
+    rospy.spin()
