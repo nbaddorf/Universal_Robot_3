f077dd207b647035a615d68f550b21ad688bfd08
Nicholas <nbaddorf@gmail.com>
2023-12-09 18:17:37 -0500

    added plain docker for robot

---
+++ ur3robot/Plain_Docker/build_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker image build -t ur3robot_plain_docker_image .
---
+++ ur3robot/Plain_Docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+export ROS_HOSTNAME=ur3robot.local
+export ROS_MASTER_URI=http://ur3robot.local:11311
+source /opt/ros/melodic/setup.bash
+
+echo "Provided arguments: $@"
+
+exec $@
