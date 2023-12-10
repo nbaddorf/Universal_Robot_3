a2f2c0d1156b1b0da909254a1e96aa68aaf3da10
Nicholas <nbaddorf@gmail.com>
2023-12-10 15:57:28 -0500

    testing docker stuff networking

--- ur3master/mac/Plain_Ros_Docker/entrypoint.sh
+++ ur3master/mac/Plain_Ros_Docker/entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 source /opt/ros/melodic/setup.bash
-#export ROS_HOSTNAME=Nicks-MBP #For some reason the robot can see dns from router, but my docker cant see ur3robot dns.
+export ROS_HOSTNAME=Nicks-MBP #For some reason the robot can see dns from router, but my docker cant see ur3robot dns.
 export ROS_MASTER_URI=http://ur3robot.local:11311
 
 #exec supervisord -c /supervisord.conf &
