c4eda6ecd5e73ea7e47184e50c45b9aa7442f18c
Nicholas <nbaddorf@gmail.com>
2023-12-16 12:18:35 -0500

    trying to call icp action message

---
+++ ur3robot/ros_code/python/call_action.py
@@ -0,0 +1,41 @@
+import rospy
+import actionlib
+
+from rail_mesh_icp.msg import MatchTemplateAction
+from rail_mesh_icp.msg import MatchTemplateActionFeedback
+from rail_mesh_icp.msg import MatchTemplateActionGoal
+from rail_mesh_icp.msg import MatchTemplateActionResult
+from rail_mesh_icp.msg import MatchTemplateFeedback
+from rail_mesh_icp.msg import MatchTemplateGoal
+from rail_mesh_icp.msg import MatchTemplateResult
+
+def call_actionlib():
+    # Creates the SimpleActionClient, passing the type of the action
+    # (FibonacciAction) to the constructor.
+    client = actionlib.SimpleActionClient('call_template', rail_mesh_icp.msg.MatchTemplateAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = rail_mesh_icp.msg.MatchTemplateGoal()
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()  # A FibonacciResult
+
+if __name__ == '__main__':
+    try:
+        # Initializes a rospy node so that the SimpleActionClient can
+        # publish and subscribe over ROS.
+        rospy.init_node('fibonacci_client_py')
+        result = call_actionlib()
+        print("Result:", ', '.join([str(n) for n in result.sequence]))
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
