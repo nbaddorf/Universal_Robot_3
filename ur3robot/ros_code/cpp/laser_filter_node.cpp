220974ad6e65a9ed88e82241eee62a41c8e33741
Nicholas <nbaddorf@gmail.com>
2024-01-02 09:58:41 -0500

    Making my own laser filter with cpp

---
+++ ur3robot/ros_code/cpp/laser_filter_node.cpp
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <sensor_msgs/LaserScan.h>
+#include <cmath>
+
+// std::vector<float>
+
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr &msg)
+{
+    ROS_INFO("I heard: [%s]", msg->ranges.c_str());
+    // std::vector<double> laser_array
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "laser_filter");
+
+    ros::NodeHandle n;
+
+    ros::Subscriber laser_sub = n.subscribe("scan", 10, laserCallback);
+
+    ros::spin();
+
+    return 0;
+}
