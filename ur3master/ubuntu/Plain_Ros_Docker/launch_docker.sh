31aaa31d53e30edffcd340b420d04ee88ac031af
Nicholas <nbaddorf@gmail.com>
2024-02-13 07:44:07 -0500

    Fixed naming issue

--- ur3master/ubuntu/Plain_Ros_Docker/launch_docker.sh
+++ ur3master/ubuntu/Plain_Ros_Docker/launch_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 docker run -it --user ros --net host --add-host nick-ros-master:127.0.0.1 -v /home/$USER/GitHub/Universal_Robot_3/ur3master/ros_code:/home/ros/catkin_ws/src/ur3master ur3master_plain_docker_image
+#--ipc=host
+#docker run -it --user ros --net host --ipc=host --add-host nick-ros-master:127.0.0.1 -v /home/$USER/GitHub/Universal_Robot_3/ur3master/ros_code:/home/ros/catkin_ws/src/ur3master ur3master_plain_docker_image
