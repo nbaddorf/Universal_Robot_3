505b9b7bdeec024fdf0b31d9d39ba775a15cad4a
Nicholas <nbaddorf@gmail.com>
2024-02-28 15:12:55 -0500

    Update scan_matcher_to_pose_stamped.cpp

--- ur3robot/ros_code/cpp/scan_matcher_to_pose_stamped.cpp
+++ ur3robot/ros_code/cpp/scan_matcher_to_pose_stamped.cpp
@@ -27,7 +27,8 @@ void pos_sub_callback(const geometry_msgs::Pose2D &msg) {
     poseStamped.pose.pose.orientation.z = theta_quat[2];
     poseStamped.pose.pose.orientation.w = theta_quat[3];
     //auto covArray(36, 0.01);
-    poseStamped.pose.covariance = {0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, };//covArray;
+    double covArray(36, 0.01);
+    poseStamped.pose.covariance = covArray; {//0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, };//covArray;
     
     pose_pub.publish(poseStamped);
   }
