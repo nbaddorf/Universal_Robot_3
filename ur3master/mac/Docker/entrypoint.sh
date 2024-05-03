1ef3c47e2be3d57a5dd7f4631600bfe25848167d
Nicholas <nbaddorf@gmail.com>
2024-05-03 14:31:49 -0400

    got mac docker to work now

--- ur3master/mac/Docker/entrypoint.sh
+++ ur3master/mac/Docker/entrypoint.sh
@@ -3,9 +3,10 @@
 set -e
 
 source /opt/ros/melodic/setup.bash
-export ROS_HOSTNAME=Nicks-MBP #For some reason the robot can see dns from router, but my docker cant see ur3robot dns.
-export ROS_MASTER_URI=http://ur3robot.local:11311
 
+export ROS_HOSTNAME=nicks-mbp #For some reason the robot can see dns from router, but my docker cant see ur3robot dns.
+export ROS_MASTER_URI=http://ur3robot.local:11311 # THIS WORKS, BUT YOU HAVE TO TURN OFF DNS ADDBLOCKER
+#export ROS_IP=192.168.86.25
 exec supervisord -c /supervisord.conf &
 
 echo "Provided arguments: $@"
--- ur3master/mac/Plain_Ros_Docker/entrypoint.sh
+++ ur3master/mac/Plain_Ros_Docker/entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 
 source /opt/ros/melodic/setup.bash
-export ROS_HOSTNAME=nicks-mbp #For some reason the robot can see dns from router, but my docker cant see ur3robot dns.
+#export ROS_HOSTNAME=nicks-mbp #For some reason the robot can see dns from router, but my docker cant see ur3robot dns.
 export ROS_MASTER_URI=http://ur3robot.local:11311 # for some reason, docker on mac works with .local but not router .
-
+export ROS_IP=192.168.86.25
 #exec supervisord -c /supervisord.conf &
 
 #echo "Provided arguments: $@"
