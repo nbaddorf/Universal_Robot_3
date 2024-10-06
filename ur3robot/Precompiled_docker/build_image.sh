5f6a65c87cc117ca16847ae5478436990b405700
Nicholas <nbaddorf@gmail.com>
2024-10-06 07:33:41 -0400

    working on precompiled dockerfile

--- ur3robot/Precompiled_docker/build_image.sh
+++ ur3robot/Precompiled_docker/build_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+tar -cvzf ros_code_archive.tar.gz ../ros_code
 docker image build -t ur3robot_precompiled_docker_image .
--- ur3robot/Precompiled_docker/entrypoint.sh
+++ ur3robot/Precompiled_docker/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-export ROS_HOSTNAME=ur3robot #.local
+export ROS_HOSTNAME=ur3robot                #.local
 export ROS_MASTER_URI=http://ur3robot:11311 #was .local
 source /opt/ros/melodic/setup.bash
 
